920b0e231754ab657c5fafb7cf52427a7a10da5b
Davi Kawasaki <davishinjik@gmail.com>
2018-01-04 23:58:12 -0200

    Renderer for main to manipulate playButton with timer module

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,8 +1,27 @@
 // Inter-process communication with main.js
 const { ipcRenderer } = require('electron');
+const timer = require('./timer');
 
 let aboutLink = document.querySelector('#link-about');
+let playButton = document.querySelector('.play-button');
+let timerSpan = document.querySelector('.time');
 
 aboutLink.addEventListener('click', function() {
     ipcRenderer.send('open-about-window');
+});
+
+let imgs = ['img/play-button.svg', 'img/stop-button.svg'];
+let play = false;
+
+playButton.addEventListener('click', function() {
+    if(play) {
+        timer.stop();
+        play = false;
+    } else {
+        timer.start(timerSpan);
+        play = true;
+    }
+
+    imgs = imgs.reverse();
+    playButton.src = imgs[0];
 });
