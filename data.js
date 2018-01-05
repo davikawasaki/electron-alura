c31451be6c3514558cfff333bc1278415c0972df
Davi Kawasaki <davishinjik@gmail.com>
2018-01-05 01:48:51 -0200

    Data manipulation on files with jsonfile and fs

---
+++ data.js
@@ -0,0 +1,43 @@
+const jsonfile = require('jsonfile-promised');
+const fs = require('fs');
+const moment = require('moment');
+const timer = require('./app/js/timer');
+
+module.exports = {
+
+    saveData(course, studiedTime) {
+        let coursePath = `${__dirname}/data/${course}.json`;
+        if(fs.existsSync(coursePath)) {
+            this.addTimeToCourseFile(coursePath, studiedTime)
+                .then(() => console.log(`[${timer.getActualFormattedDate()}] Curso ${course} com tempo ${studiedTime} atualizado com sucesso!`))
+                .catch((err) => console.error(err));;
+        } else {
+            this.createCourseArchive(coursePath, {})
+                .then(() => {
+                    console.log(`[${timer.getActualFormattedDate()}] Arquivo referente ao curso ${course} criado com sucesso!`);
+                    return this.addTimeToCourseFile(coursePath, studiedTime);
+                })
+                .then(() => console.log(`[${timer.getActualFormattedDate()}] Curso ${course} com tempo ${studiedTime} inserido com sucesso!`))
+                .catch((err) => console.error(err));
+        };
+    },
+
+    addTimeToCourseFile(fileFullPath, studiedTime) {
+        let data = {
+            lastSave: new Date().toString(),
+            time: studiedTime
+        };
+
+        return jsonfile.writeFile(fileFullPath, data, {spaces: 2});
+    },
+
+    createCourseArchive(fileFullPath, fileContent) {
+        return jsonfile.writeFile(fileFullPath, fileContent);
+    },
+
+    getData(course) {
+        let coursePath = `${__dirname}/data/${course}.json`;
+        return jsonfile.readFile(coursePath);
+    }
+
+}
