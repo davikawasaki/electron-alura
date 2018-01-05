a05c43db00a7bcd2a6184f00cff63d17d9852c9d
Davi Kawasaki <davishinjik@gmail.com>
2018-01-04 22:45:44 -0200

    Renderer for about page event handler in main

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,1 +1,8 @@
-console.log('Developer tools!');
+// Inter-process communication with main.js
+const { ipcRenderer } = require('electron');
+
+let aboutLink = document.querySelector('#link-about');
+
+aboutLink.addEventListener('click', function() {
+    ipcRenderer.send('open-about-window');
+});
